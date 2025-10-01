53964b40dbceb4742ed9430149badd6dc23235ab
amer585 <ameremadapdelkalek@gmail.com>
2025-10-01 23:02:51 +0300

    Update ai_model.py

--- backend/ai_model.py
+++ backend/ai_model.py
@@ -1,1 +1,14 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class TransferResNet(nn.Module):
+    def __init__(self, num_classes, pretrained=True):
+        super().__init__()
+        self.backbone = models.resnet18(pretrained=pretrained)
+        in_features = self.backbone.fc.in_features
+        self.backbone.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.backbone(x)
 
